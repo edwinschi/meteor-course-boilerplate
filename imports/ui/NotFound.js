713713097392d0775dcae1d6d5dac352c11ed52f
Edwin Schissato <edwinschi@gmail.com>
2017-12-08 00:04:31 -0200

    Init Commit

---
+++ imports/ui/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Link } from 'react-router';
+export default () => {
+    return (
+        <div className="boxed-view">
+            <div className="boxed-view__box">
+                <h1>Page not Found</h1>
+                <p>We're unable to find that page.</p>
+                <Link to="/" className="button button--link">HEAD HOME</Link>
+            </div>
+        </div>
+    );
+};
---
+++ readme.md
@@ -0,0 +1,7 @@
+# Boilerplate Meteor Project    
+
+This is a basic react and auth setup
+
+## Authors
+
+Edwin Schissato
